26cb51fd355ac1bff94144ed8b42052f1fd65644
krilinxito <barryallen4207@gmail.com>
2024-12-10 16:53:38 -0400

    subiendo

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App titulo='Hola Mundo' img='https://media.tenor.com/ApvFlyMJY3AAAAAe/shrek-diva.png' style='texto' h1='titulo'/>
+    <br />
+    <App titulo='Adios Mundo' img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC-ly5LASWD8SIrkVcZmLFATcLFwnS4Z-azA&s' style='texto'  h1='titulo1' />
+    <br />
+    <App titulo='Hola de nuevo mundo' img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl6sPRO-p1R2d1eKBn5PdXGR88zzy0aJ7mkA&s' style='texto'  h1='titulo2' />
   </StrictMode>,
 )
